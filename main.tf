29d16825805e35051ed1954482844445f05dd130
hardikmehta9 <hardik0260.be20@chitkara.edu.in>
2024-07-27 20:58:51 +0530

    normal_way

---
+++ main.tf
@@ -0,0 +1,156 @@
+resource "newrelic_alert_policy" "Latestpolicy" {
+  name = "Latestpolicy"
+}
+
+resource "newrelic_nrql_alert_condition" "newcondition" {
+  account_id                     = 4438271
+  policy_id                      = newrelic_alert_policy.Latestpolicy.id
+  type                           = "static"
+  name                           = "Alertnew"
+  description                    = "Alert when transactions are taking too long"
+  runbook_url                    = "https://www.example.com"
+  enabled                        = true
+  violation_time_limit_seconds   = 3600
+  fill_option                    = "static"
+  fill_value                     = 1.0
+  aggregation_window             = 60
+  aggregation_method             = "event_flow"
+  aggregation_delay              = 120
+  expiration_duration            = 120
+  open_violation_on_expiration   = true
+  close_violations_on_expiration = true
+  slide_by                       = 30
+
+  nrql {
+    query = "SELECT average(largestContentfulPaint) FROM PageViewTiming WHERE appName = 'react-cart' "
+ 
+
+  }
+
+  critical {
+    operator              = "above"
+    threshold             = 5.5
+    threshold_duration    = 300
+    threshold_occurrences = "ALL"
+  }
+
+  warning {
+    operator              = "above"
+    threshold             = 3.5
+    threshold_duration    = 600
+    threshold_occurrences = "ALL"
+  }
+}
+resource "newrelic_nrql_alert_condition" "newcondition2" {
+  account_id                     = 4438271
+  policy_id                      = newrelic_alert_policy.Latestpolicy.id
+  type                           = "static"
+  name                           = "Alertnew1"
+  description                    = "Alert when transactions are taking too long"
+  runbook_url                    = "https://www.example.com"
+  enabled                        = true
+  violation_time_limit_seconds   = 3600
+  fill_option                    = "static"
+  fill_value                     = 1.0
+  aggregation_window             = 60
+  aggregation_method             = "event_flow"
+  aggregation_delay              = 120
+  expiration_duration            = 120
+  open_violation_on_expiration   = true
+  close_violations_on_expiration = true
+  slide_by                       = 30
+
+  nrql {
+    query = "SELECT percentile(cumulativeLayoutShift ) FROM PageViewTiming WHERE appName = 'react-cart'"
+  }
+
+  critical {
+    operator              = "above"
+    threshold             = 5.5
+    threshold_duration    = 300
+    threshold_occurrences = "ALL"
+  }
+
+  warning {
+    operator              = "above"
+    threshold             = 3.5
+    threshold_duration    = 600
+    threshold_occurrences = "ALL"
+  }
+}
+
+resource "newrelic_nrql_alert_condition" "newcondition3" {
+  account_id                     = 4438271
+  policy_id                      = newrelic_alert_policy.Latestpolicy.id
+  type                           = "static"
+  name                           = "Alertnew2"
+  description                    = "Alert when transactions are taking too long"
+  runbook_url                    = "https://www.example.com"
+  enabled                        = true
+  violation_time_limit_seconds   = 3600
+  fill_option                    = "static"
+  fill_value                     = 1.0
+  aggregation_window             = 60
+  aggregation_method             = "event_flow"
+  aggregation_delay              = 120
+  expiration_duration            = 120
+  open_violation_on_expiration   = true
+  close_violations_on_expiration = true
+  slide_by                       = 30
+
+  nrql {
+    query = "SELECT average(domProcessingDuration ) AS 'Dom processing'FROM PageView WHERE appName = 'react-cart'"  
+  }
+
+  critical {
+    operator              = "above"
+    threshold             = 5.5
+    threshold_duration    = 300
+    threshold_occurrences = "ALL"
+  }
+
+  warning {
+    operator              = "above"
+    threshold             = 3.5
+    threshold_duration    = 600
+    threshold_occurrences = "ALL"
+  }
+}
+
+resource "newrelic_nrql_alert_condition" "newcondition4" {
+  account_id                     = 4438271
+  policy_id                      = newrelic_alert_policy.Latestpolicy.id
+  type                           = "static"
+  name                           = "Alertnew3"
+  description                    = "Alert when transactions are taking too long"
+  runbook_url                    = "https://www.example.com"
+  enabled                        = true
+  violation_time_limit_seconds   = 3600
+  fill_option                    = "static"
+  fill_value                     = 1.0
+  aggregation_window             = 60
+  aggregation_method             = "event_flow"
+  aggregation_delay              = 120
+  expiration_duration            = 120
+  open_violation_on_expiration   = true
+  close_violations_on_expiration = true
+  slide_by                       = 30
+
+  nrql {
+    query = "SELECT percentage(count(*),WHERE error is true) FROM PageView WHERE appName = 'react-cart'"
+  }
+
+  critical {
+    operator              = "above"
+    threshold             = 5.5
+    threshold_duration    = 300
+    threshold_occurrences = "ALL"
+  }
+
+  warning {
+    operator              = "above"
+    threshold             = 3.5
+    threshold_duration    = 600
+    threshold_occurrences = "ALL"
+  }
+}
